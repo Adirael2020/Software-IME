33a2bc3c0720467fcc3c3bf19be6312e37a4eae3
Matias Alejandro Gramajo <matiasgramajo@live.com>
2023-08-21 19:51:29 -0300

    comit

--- src/controllers/user.controler.js
+++ src/controllers/user.controler.js
@@ -1,3 +1,24 @@
+import { user } from "../models/user.model";
+
 export const createUser = (req,res) => {
-    
-}
+    const {
+        username,
+        password,
+        fullname,
+        email,
+        hierarchy,
+        specialty,
+        birthday
+    } = req.body;
+
+    const newUser = new user ({
+        username,
+        password,
+        fullname,
+        email,
+        hierarchy,
+        specialty,
+        birthday
+    });
+
+};
