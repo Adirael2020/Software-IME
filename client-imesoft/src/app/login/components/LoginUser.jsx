70cba3477d414e420a33ab313b80209f3f94716b
Lourdes Sarmiento <66499325+LourSarmiento@users.noreply.github.com>
2023-09-12 21:54:39 -0300

    login

--- client-imesoft/src/app/login/components/LoginUser.jsx
+++ client-imesoft/src/app/login/components/LoginUser.jsx
@@ -64,16 +64,16 @@ const LoginUser = () => {
             required: { value: true, message: "Se requiere nombre de Usuario" },
           })}
         />
-        <p className="text-red-700">{formErrors.username?.message}</p>
+        <p className="text-red-700 my-4 text-center">{formErrors.username?.message}</p>
         <input
           type="password"
           name="password"
           placeholder="Contraseña"
           className="w-full focus:outline-none appearance-none bg-transparent placeholder:italic placeholder:text-slate-600  border-b-2 border-slate-700 text-white px-4 py-2 my-4"
-          {...register("password", { required: { value: true, message: "Se requiere Contraseña" }, minLength: { value: 6, message: "La contraseña debe ser de 6 caracteres" } })}
+          {...register("password", { required: { value: true, message: "Se requiere Contraseña" }, minLength: { value: 6, message: "La contraseña debe ser de 6 caracteres"} })}
         />
-        <p className="text-red-700">{formErrors.password?.message}</p>
-        <div className="flex items-center justify-center my-4">
+        <p className="text-red-700 my-4 text-center">{formErrors.password?.message}</p>
+        <div className="flex items-center justify-center ">
           <button className="bg-slate-900 text-white w-1/2 font-medium hover:bg-slate-800 p-2 rounded-full">
             Ingresar
           </button>
