2ce06ec1757522d2e87d023e7039587b80a4991c
Lourdes Sarmiento <66499325+LourSarmiento@users.noreply.github.com>
2023-09-24 18:11:25 -0300

    ffr

--- client-imesoft/src/app/login/components/Images.jsx
+++ client-imesoft/src/app/login/components/Images.jsx
@@ -1,10 +1,25 @@
 import Image from 'next/image';
+import styles from "../../styles/login.module.css"
 
 function Images() {
   return (
+    <div className={styles.conteiner}>
+     <ul className='styles.slider'>
+      <li className='styles.slide' id='slider1'>
     <div className='md:shrink-0 opacity-70'>
-      <Image src="/feed.jpg" alt="Feed" width={367} height={200} />
+      <Image src="/feed.jpg" alt="Feed" width={367} height={300} />
     </div>
+    </li>
+    <li className='styles.slide' id='slider1'>
+    <div className='md:shrink-0 opacity-70'>
+    <Image src="/feed_2.jpg" alt="Feed" width={367} height={200} />
+  </div></li>
+  <li className='styles.slide' id='slider1'>
+    <div className='md:shrink-0 opacity-70'>
+      <Image src="/feed_3 (1).jpg" alt="Feed" width={367} height={200} />
+  </div></li>
+  </ul>
+  </div>
   );
 }
 
