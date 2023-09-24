cf4e0e160e4e422272470c920f13c98bc3bee280
Matias Alejandro Gramajo <matiasgramajo@live.com>
2023-09-24 19:10:32 -0300

    comit

--- client-imesoft/src/app/homePage/users/components/UserForm.jsx
+++ client-imesoft/src/app/homePage/users/components/UserForm.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+"use client"
+
+import { useEffect, useState } from "react";
+import { useRouter, useParams } from "next/navigation";
+//Formulario
+import { useForm } from "react-hook-form";
+//Components 
+import Button from "../../../../components/Button.jsx";
+//redux
+
 
 const UserForm = () => {
+
+  const navigate = useRouter();
+  const params = useParams();
+
+  //React Hook Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors: formErrors },
+    reset
+  } = useForm();
+  //Messange
+  const [message, setMessage] = useState();
+
   return (
     <div>UserForm</div>
   )
--- client-imesoft/src/app/homePage/users/page.jsx
+++ client-imesoft/src/app/homePage/users/page.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+"use client"
+import { useRouter } from "next/navigation";
+//components
+import Button from "../../../components/Button";
 
 const UserPage = () => {
+
+  const navigate = useRouter();
+
   return (
-    <div>Lista de Usuarios</div>
+    <div>
+      <div>
+        Lista de Usuarios
+      </div>
+      <Button text={"Nuevo Usuario"} className="bg-green-600 p-2" onClick={()=>{navigate.push('/homePage/users/newUser');}} />
+    </div>
   )
 }
 
