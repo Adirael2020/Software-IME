a7160d623e3b8e24ef372692a4e92e684b194b4d
Matias Alejandro Gramajo <matiasgramajo@live.com>
2023-11-06 13:50:31 -0300

    comit

--- src/s3.js
+++ src/s3.js
@@ -1,5 +1,6 @@
-import { S3Client } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import {AWS_BUCKET_NAME,AWS_BUCKET_REGION,AWS_PUBLIC_KEY,AWS_SECRECT_KEY} from './config.js';
+import fs from 'fs';
 
 const client = new S3Client({
     region: AWS_BUCKET_REGION,
@@ -9,6 +10,14 @@ const client = new S3Client({
     }
 });
 
-function uploadFile(file){
-    
+export async function uploadFile(file){
+    const stream = fs.createReadStream(file);
+    const uploadParams = {
+        Bucket: AWS_BUCKET_NAME,
+        Key: 'Hola.png',
+        Body: stream
+    };
+    const command = new PutObjectCommand(uploadParams);
+    const result = await client.sent(command);
+    console.log(result);
 }
