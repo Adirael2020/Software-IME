479048cbd4759890f2d5939e4993aa5983be8bcb
Matias Alejandro Gramajo <matiasgramajo@live.com>
2023-11-10 13:56:00 -0300

    comit

--- src/routes/User.routes.js
+++ src/routes/User.routes.js
@@ -5,18 +5,19 @@ import { authRequired } from "../middlewares/validateUserToken.js";
 
 //Zod
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
+import { userSchema,editPasswordUserSchema,editUserSchema } from "../schemas/user.schema.js";
 
 const router = Router();
 
 //get all
 router.get("/getUsers",authRequired,getUsers);
 //create
-router.post("/createUser",authRequired,createUser);
+router.post("/createUser",authRequired,validateSchema(userSchema),createUser);
 //get user
 router.post("/getUser/:id",authRequired,getUser);
 //edit user
-router.put("/editUser/:id",authRequired, editUser);
-router.put("/editProfileUser/:id",authRequired,editProfile);
+router.put("/editUser/:id",authRequired,validateSchema(userSchema),editUser);
+router.put("/editProfileUser/:id",authRequired,validateSchema(editUserSchema),editProfile);
 //active user
 router.put("/activateUser/:id",authRequired,activateUser);
 //deactivate user
@@ -24,7 +25,7 @@ router.put("/deactivateUser/:id",authRequired,deactivateUser);
 //reset password user
 router.put("/resetPassword/:id",authRequired,resetPassword);
 //edit password user
-router.put("/editPassword/:id",authRequired,editPasswordProfile);
+router.put("/editPassword/:id",authRequired,validateSchema(editPasswordUserSchema),editPasswordProfile);
 
 
 export default router
--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,6 +1,6 @@
 import {z} from 'zod';
 
-export const registerUserSchema = z.object({
+export const userSchema = z.object({
     username: z.string({
         required_error: 'Username is required'
     }),
@@ -25,5 +25,31 @@ export const registerUserSchema = z.object({
         required_error: "Please select a date and time",
         invalid_type_error: "That's not a date!",
       }),
-    headqueartersFromUser: z.string().array()
+    headqueartersFromUser: z.string().array(),
+    isTeacher: z.boolean({
+        required_error: 'not a boolean'
+    })
 });
+
+export const editUserSchema = z.object({
+    fullname: z.string({
+        required_error: 'Fullname is required'
+    }),
+    email: z.string({
+        required_error: 'Email is required'
+    }).email({
+        required_error: 'Ivalid email'
+    }),
+    birthday: z.date({
+        required_error: "Please select a date and time",
+        invalid_type_error: "That's not a date!",
+      }),
+});
+
+export const editPasswordUserSchema = z.object({
+    password: z.string({
+        required_error: 'Password is required'
+    }).min(6,{
+        message: 'Password must be at least 6 characters'
+    }),
+})
